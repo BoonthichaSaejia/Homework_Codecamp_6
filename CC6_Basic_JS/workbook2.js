8c389f514c85116d3aed77e2ab064dc3a2024aa9
Boonthicha Saejia <boonthicha.s@gmail.com>
2020-07-21 14:01:12 +0700

    add JS01.html and edit workbook2.js

--- CC6_Basic_JS/workbook2.js
+++ CC6_Basic_JS/workbook2.js
@@ -1,11 +1,11 @@
 function changeColor(){
-    alert("Change Color Function...")
-    let tcolor = document.querySelector("#int1").value
-    let bcolor = document.querySelector("#int2").value
+    alert("Change Color Function...");
+    let tcolor = document.querySelector("#int1").value;
+    let bcolor = document.querySelector("#int2").value;
     // alert(tcolor)
     // alert(bcolor)
-    document.querySelector("h2").style.color=tcolor
-    document.querySelector("h2").style.backgroundColor=bcolor
+    document.querySelector("h2").style.color=tcolor;
+    document.querySelector("h2").style.backgroundColor=bcolor;
 
 }
     
